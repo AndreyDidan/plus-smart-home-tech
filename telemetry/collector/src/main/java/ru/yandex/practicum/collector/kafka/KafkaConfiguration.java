abf49a032883e4975c05bdfc29027a819794e2da
Andrey Didan <andrey_didan@mail.ru>
2025-04-10 01:52:38 +0300

    Вторая итерация

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/kafka/KafkaConfiguration.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/kafka/KafkaConfiguration.java
@@ -4,7 +4,7 @@ import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.VoidSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.yandex.practicum.kafka.serializer.GeneralAvroSerializer;
@@ -25,7 +25,7 @@ public class KafkaConfiguration {
                 if (producer == null) {
                     Properties properties = new Properties();
                     properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-                    properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, VoidSerializer.class);
+                    properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
                     properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class);
                     producer = new KafkaProducer<>(properties);
                 }
