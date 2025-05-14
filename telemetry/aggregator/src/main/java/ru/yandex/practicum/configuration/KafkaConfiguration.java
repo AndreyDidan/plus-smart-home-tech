37d8c6da8bde0a0be1cfa897be1970522511cf4b
Andrey Didan <andrey_didan@mail.ru>
2025-05-14 12:11:10 +0300

    Исправил замечания по aggregator

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/configuration/KafkaConfiguration.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/configuration/KafkaConfiguration.java
@@ -4,13 +4,8 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.yandex.practicum.kafka.deserializer.SensorEventDeserializer;
-import ru.yandex.practicum.kafka.serializer.GeneralAvroSerializer;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 
@@ -18,17 +13,20 @@ import java.util.Properties;
 
 @Configuration
 public class KafkaConfiguration {
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
+    private final KafkaProperties kafkaProperties;
+
+    public KafkaConfiguration(KafkaProperties kafkaProperties) {
+        this.kafkaProperties = kafkaProperties;
+    }
 
     @Bean
     public KafkaConsumer<String, SensorEventAvro> kafkaConsumer() {
         Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "aggregator-group");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class.getName());
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaProperties.getConsumer().getGroupId());
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, kafkaProperties.getConsumer().getKeyDeserializer());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, kafkaProperties.getConsumer().getValueDeserializer());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, kafkaProperties.getConsumer().getAutoOffsetReset());
 
         return new KafkaConsumer<>(props);
     }
@@ -36,9 +34,9 @@ public class KafkaConfiguration {
     @Bean
     public KafkaProducer<String, SensorsSnapshotAvro> kafkaProducer() {
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class.getName());
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, kafkaProperties.getProducer().getKeySerializer());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, kafkaProperties.getProducer().getValueSerializer());
 
         return new KafkaProducer<>(props);
     }
--- telemetry/aggregator/src/main/java/ru/yandex/practicum/service/AggregationStarter.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/service/AggregationStarter.java
@@ -9,6 +9,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.errors.WakeupException;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.configuration.KafkaProperties;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 
@@ -23,15 +24,10 @@ public class AggregationStarter {
     private final KafkaConsumer<String, SensorEventAvro> consumer;
     private final KafkaProducer<String, SensorsSnapshotAvro> producer;
     private final SnapshotAggregator aggregator;
+    private final KafkaProperties kafkaProperties;
 
-    private static final String SNAPSHOT_TOPIC = "telemetry.snapshots.v1";
-    /**
-     * Метод для начала процесса агрегации данных.
-     * Подписывается на топики для получения событий от датчиков,
-     * формирует снимок их состояния и записывает в кафку.
-     */
     public void start() {
-        consumer.subscribe(List.of("telemetry.sensors.v1"));
+        consumer.subscribe(List.of(kafkaProperties.getTopics().getSensors()));
         try {
             while (true) {
                 ConsumerRecords<String, SensorEventAvro> records = consumer.poll(Duration.ofMillis(500));
@@ -46,11 +42,11 @@ public class AggregationStarter {
                     Optional<SensorsSnapshotAvro> snapshotOpt = aggregator.updateState(event);
                     snapshotOpt.ifPresent(snapshot -> {
                         log.info("Обновлен снимок состояния: {}", snapshot);
-                        producer.send(new ProducerRecord<>(SNAPSHOT_TOPIC, snapshot.getHubId(), snapshot), (metadata, exception) -> {
+                        producer.send(new ProducerRecord<>(kafkaProperties.getTopics().getSnapshots(), snapshot.getHubId(), snapshot), (metadata, exception) -> {
                             if (exception != null) {
                                 log.error("Ошибка отправки в Kafka: {}", exception.getMessage());
                             } else {
-                                log.info("Сообщение успешно отправлено в Kafka в топик {}", SNAPSHOT_TOPIC);
+                                log.info("Сообщение успешно отправлено в Kafka в топик {}", kafkaProperties.getTopics().getSnapshots());
                             }
                         });
                     });
@@ -62,11 +58,9 @@ public class AggregationStarter {
         } catch (Exception e) {
             log.error("Ошибка во время обработки событий от датчиков", e);
         } finally {
-
             try {
                 producer.flush();
                 consumer.commitSync();
-
             } finally {
                 log.info("Закрываем консьюмер");
                 consumer.close();
@@ -76,4 +70,3 @@ public class AggregationStarter {
         }
     }
 }
-
